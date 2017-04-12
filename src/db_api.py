3c019801c11bf694a67fbea70246c21b41c6b865
Lari-Matias Orjala <lorjala@student.oulu.fi>
2017-04-12 19:53:57 +0300

    Updated database api to use sqlalchemy and abstracted ORM.

--- src/db_api.py
+++ src/db_api.py
@@ -1,45 +1,36 @@
 from __future__ import print_function
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, String, Integer, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sys import exit
 
-import psycopg2
+from dbconfig import DB_CONFIG
 
-import dbconfig
+# Tables
+from datatype import DataType
 
 class LightSenseDatabase(object):
 
-    def __init__(self, user, pw, host, port, dbname):
-        self.user = user
-        self.pw = pw
-        self.host = host
-        self.port = port
-        self.dbname = dbname
-
-        self.conn_string = "user={} password={} host={} port={} dbname={}"
-        self.conn_string = self.conn_string.format(self.user, self.pw, self.host, self.port, self.dbname)
-        self.conn = self.connect_to_db(self.conn_string)
-
-
-    def connect_to_db(self, connection_string):
+    def __init__(self, config):
         try:
-            conn = psycopg2.connect(connection_string)
+            db_string = "postgresql://{USER}:{PASSW}@{HOST}:{PORT}/{DBNAME}".format(**config)
+            self.engine = create_engine(db_string)
+            self.base = declarative_base()
+            self.base.metadata.create_all(self.engine)
+            self.session = sessionmaker(self.engine)()
         except:
-            print("Cannot connect to the database, check that the settings are OK:\n", connection_string)
-        return conn
+            print("Cannot connect to the database, please check that the settings are configured correctly.")
+            exit()
 
-    def get_datatypes(self, conn):
-        result = []
-        # http://initd.org/psycopg/docs/usage.html#with-statement
-        with conn:  # Not quite sure if this is needed here...
-            with conn.cursor() as cur:
-                sql = 'SELECT * FROM "DataType" ORDER BY "Id";'
-                cur.execute(sql)
-                result = cur.fetchall()
-        return result
-
+    def get_datatypes(self):
+        return self.session.query(DataType)
 
 def main():
-    db = LightSenseDatabase(dbconfig.USER, dbconfig.PASSW, dbconfig.HOST, dbconfig.PORT, dbconfig.DBNAME)
-    records = db.get_datatypes(db.conn)
-    print(records)
+    db = LightSenseDatabase(DB_CONFIG)
+    records = db.get_datatypes()
+    for record in records:
+        print(record.description)
 
 if __name__ == "__main__":
     main()
