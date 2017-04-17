6f8caeeb3d9439930e2580d834048a2e8c816ce3
Eero Paavola <paavola.ee@gmail.com>
2017-04-17 20:13:55 +0300

    Add methods to DBapi

--- src/db_api.py
+++ src/db_api.py
@@ -51,15 +51,10 @@ class LightSenseDatabase(object):
         .filter(Measurement.node_id == Location.id) \
         .filter(Location.location.isnot(None))
 
-    '''
-        Return all events for a single node of certain type.
-        Returns a list of dicts.
-        Keys: 
-            Timestamp(datetime)
-            Measurement(string)
-            Value(float)
-    '''
     def _get_node_events_of_type(self, node_id, measurement_type_id):
+        """
+        node_id = Location.Id (not Location.NodeId)
+        """
         return [{   "Timestamp": u.timestamp.event_timestamp, 
                     "Measurement": u.measurement.measurement_type.description,
                     "Value": u.value} for u in self.session.query(Event) \
@@ -69,36 +64,82 @@ class LightSenseDatabase(object):
         .filter(Location.id == node_id) \
         .filter(Measurement.measurement_type_id == measurement_type_id)]
 
-    '''
-        Return all temperature events for a single node.
-    '''
     def get_node_temperatures(self, node_id):
         return self._get_node_events_of_type(node_id, 4)
 
-    '''
-        Return all humidity events for a single node.
-    '''
     def get_node_humidities(self, node_id):
         return self._get_node_events_of_type(node_id, 5)
 
-    '''
-        Return all cycle count events for a single node.
-    '''
     def get_node_cycle_counts(self, node_id):
         return self._get_node_events_of_type(node_id, 6)
 
-    '''
-        Return all voltage events for a single node.
-    '''
     def get_node_voltages(self, node_id):
         return self._get_node_events_of_type(node_id, 7)
 
+    def _get_node_events_of_type_by_node_id(self, node_id, measurement_type_id):
+        """
+        Return all events of certain type for a single node.
+        
+        :param node_id: int
+        :param measurement_type_id: int
+        :return: list of dicts
+        Keys: 
+            Timestamp(datetime)
+            Measurement(string)
+            Value(float)
+        """
+        return [{"Timestamp": u.timestamp.event_timestamp,
+                 "Measurement": u.measurement.measurement_type.description,
+                 "Value": u.value} for u in self.session.query(Event) \
+                    .join(Measurement).join(Location) \
+                    .filter(Event.measurement_id == Measurement.id) \
+                    .filter(Measurement.node_id == Location.id) \
+                    .filter(Location.node_id == node_id) \
+                    .filter(Measurement.measurement_type_id == measurement_type_id)]
+
+    def get_node_temperatures_by_node_id(self, node_id):
+        """
+        Return all temperature events for a single node.
+
+        :param node_id: int 
+        :return: list of all temperature events for a single node.
+        """
+        return self._get_node_events_of_type_by_node_id(node_id, 4)
+
+    def get_node_humidities_by_node_id(self, node_id):
+        """
+        Return all humidity events for a single node.
+
+        :param node_id: int 
+        :return: list of all humidity events for a single node.
+        """
+        return self._get_node_events_of_type_by_node_id(node_id, 5)
+
+    def get_node_cycle_counts_by_node_id(self, node_id):
+        """
+        Return all cycle count events for a single node.
+
+        :param node_id: int 
+        :return: list of all cycle count events for a single node.
+        """
+        return self._get_node_events_of_type_by_node_id(node_id, 6)
+
+    def get_node_voltages_by_node_id(self, node_id):
+        """
+        Return all voltage events for a single node.
+
+        :param node_id: int 
+        :return: list of all voltage events for a single node.
+        """
+        return self._get_node_events_of_type_by_node_id(node_id, 7)
+
 def main():
     db = LightSenseDatabase(DB_CONFIG)
-    print(db.get_node_temperatures(347))
-    print(db.get_node_humidities(347))
-    print(db.get_node_cycle_counts(347))
-    print(db.get_node_voltages(347))
+    temp_list = db.get_node_temperatures_by_node_id(253)
+    print(len(temp_list))
+    #print(db.get_node_humidities(347))
+    #print(db.get_node_cycle_counts(347))
+    #print(db.get_node_voltages(347))
 
 if __name__ == "__main__":
     main()
